8a8b077e55e9ed4862e94e5860b671c58820c503
Guifs <guifs100@gmail.com>
2020-05-19 01:04:00 -0300

    Teste e headhunter cria conta

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,5 +1,7 @@
-# Permitted locales available for the application
-I18n.available_locales = [:'en', :'pt-BR']
- 
-# Set default locale to something other than :en
+#Permited locales avaliable for the application
+
+# I18n.avaliable_locales = [:en, :pt-BR]
+I18n.available_locales = [:'pt-BR']
+
+#Set default locale to something other than :en
 I18n.default_locale = :'pt-BR'
--- spec/models/headhunter_spec.rb
+++ spec/models/headhunter_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Headhunter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# TODO: teste para email unico
+# RSpec.describe Headhunter, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
